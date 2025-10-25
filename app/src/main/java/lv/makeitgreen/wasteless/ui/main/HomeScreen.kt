80feb1e35dee892e04e949d41775710c1353e51f
rvndsy <112282114+rvndsy@users.noreply.github.com>
2025-10-25 17:18:20 +0300

    Add info page buttons on HomeScreen

--- app/src/main/java/lv/makeitgreen/wasteless/ui/main/HomeScreen.kt
+++ app/src/main/java/lv/makeitgreen/wasteless/ui/main/HomeScreen.kt
@@ -1,37 +1,157 @@
 package lv.makeitgreen.wasteless.ui.main
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import lv.makeitgreen.wasteless.navigation.AppDestinations
 import lv.makeitgreen.wasteless.ui.components.NavBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController) {
     var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
 
     NavBar(currentDestination, navController) {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            HelloWorld(
-                modifier = Modifier.padding(innerPadding)
+            HomeSearchBar(
+                searchBarModifier = Modifier.fillMaxWidth(0.95F)
             )
+            HomeInfoButtons()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HelloWorld(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello World!",
+fun HomeSearchBar(searchBarModifier: Modifier) {
+    var searchText by remember { mutableStateOf("") }
+    var isSearchActive by remember { mutableStateOf(false) }
+
+    Box(
+        Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        SearchBar(
+            query = searchText,
+            onQueryChange = {
+                searchText = it
+            },
+            onSearch = {
+                isSearchActive = false
+            },
+            active = isSearchActive,
+            onActiveChange = {
+                isSearchActive = it
+            },
+            placeholder = {
+                Text(
+                    text = "Search here",
+                )
+            },
+            leadingIcon = {
+                Icon(imageVector = Icons.Rounded.Search, contentDescription = "Search Icon")
+            },
+            trailingIcon = {
+                if (isSearchActive) {
+                    Icon(
+                        imageVector = Icons.Rounded.Close,
+                        contentDescription = "Close Icon",
+                        modifier = Modifier.clickable{
+                            if (searchText.isEmpty()) {
+                                isSearchActive = false
+                            } else {
+                                searchText = ""
+                            }
+                        },
+                    )
+                }
+            },
+            modifier = searchBarModifier
+        ) {}
+    }
+}
+
+@Composable
+fun HomeInfoButtons(
+    modifier: Modifier = Modifier
+) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(top = 96.dp)
+            .verticalScroll(rememberScrollState()),
+        // verticalArrangement = Arrangement.spacedBy(16.dp), //currently done by top padding within InfoButton
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val buttonHeight = 200.dp
+        InfoButton(
+            onClick = {},
+            buttonHeight = buttonHeight,
+            text = "Simboli"
+        )
+        InfoButton(
+            onClick = {},
+            buttonHeight = buttonHeight,
+            text = "Atkritumu veidi"
+        )
+        InfoButton(
+            onClick = {},
+            buttonHeight = buttonHeight,
+            text = "Atkritumu apsaimniekotāji"
+        )
+        InfoButton(
+            onClick = {},
+            buttonHeight = buttonHeight,
+            text = "(For scrolling)"
+        )
+    }
+}
+
+@Composable
+fun InfoButton(
+    onClick: () -> Unit,
+    text: String,
+    buttonHeight: Dp,
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth(0.95f)
+            .height(buttonHeight)
+            .padding(top = 16.dp),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Text(text)
+    }
 }
